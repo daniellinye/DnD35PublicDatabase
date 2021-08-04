79445fb8d57b673d6ba8a8ef8ac084fa7175ee59
s2087650 <daniellinye@hotmail.com>
2021-08-04 15:58:56 +0200

    Add project files.

---
+++ Loaders/DatabaseFetcher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using Kubernetes1.Loaders.Models;
+
+namespace Kubernetes1.Loaders.DatabaseFetcher
+{
+    public class DatabaseFetcher : DbContext
+    {
+        public DbSet<SourceLoader> Sources { get; set; }
+        public DbSet<ClassLoader> Classes { get; set; }
+        public DbSet<SpellLoader> Spells { get; set; }
+        public DbSet<ItemLoader> Items { get; set; }
+
+        
+        public DatabaseFetcher():base("DataListings")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Loaders/Models/SourceLoader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kubernetes1.Loaders.Models
+{
+    public class SourceLoader
+    {
+        public int id { get; set; }
+        public char[] acronym { get; set; }
+        public int page { get; set; }
+    }
+}
